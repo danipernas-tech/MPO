64d394ffcdd586e0b47c351b96cf347d8296f003
danipernas-tech <danielpernasbenito@gmail.com>
2026-03-16 19:59:04 +0100

    Add temperature evaluation logic to sesion03.py for cooking chicken

--- sesion03.py
+++ sesion03.py
@@ -1,3 +1,15 @@
 
 #Ejercicios unidad 01 clase
 
+print("Dime la temperatura en la que estas cocinando el pollo\n")
+temperatura = float(input())
+
+# 0-60 no se cocina
+# 60 -200 optima
+# >200 se quema
+if temperatura > 200.0:
+    print("Se quema")
+elif temperatura > 60.0:
+    print("Se quema")
+elif temperatura < 60.0:
+    print("No se cocina")
