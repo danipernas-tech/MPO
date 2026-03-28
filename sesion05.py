528aef52bc708aada5ceaf9f2843ae3cbc60a4c8
danipernas-tech <danielpernasbenito@gmail.com>
2026-03-28 17:17:22 +0100

    Add sesion05.py file with control flow structures examples

---
+++ sesion05.py
@@ -0,0 +1,31 @@
+
+#ESTRUCTURAS DE CONTROL DE FLUJO
+
+#Estructura if
+edad = 22
+
+if edad >= 18:
+    print(f"Eres mayor de edad, tienes {edad} años")
+
+
+#Estructura elif
+
+if edad >=18:
+    print(f"Eres mayor de edad, tienes {edad} años")
+elif edad < 18:
+    print("Eres menor de edad")
+
+
+#Estructura else
+
+if edad > 65:
+    print(f"Eres muy mayor, tienes {edad} años")
+elif edad >= 18:
+    print("Eres menor de edad")
+else:
+    print("Eres menor de edad")
+
+
+#ESTRUCTURAS DE CONTROL DE REPETICIÓN
+
+
